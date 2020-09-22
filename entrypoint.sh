5d151b17a475899b996ddc8302ab983f3b226101
Uy Nguyen <70513886+uynguyen-wonderlabs@users.noreply.github.com>
2020-09-23 01:13:43 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 BRANCH=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g")
-IS_RELEASE_BRANCH=0
+IS_RELEASE_BRANCH='NO'
+RELEASE_VERSION=BRANCH
 
 # if contains release/
 if [ $(echo ${GITHUB_REF} | sed -e "s/release\///g") != ${GITHUB_REF} ]; then
-  IS_RELEASE_BRANCH=1
+  IS_RELEASE_BRANCH='YES'
+  RELEASE_VERSION=$(echo ${BRANCH} | sed -e "s/release\///g")
 fi;
 
 echo "::set-output name=branch::${BRANCH}"
-echo "::set-output name=isReleaseBranch::${IS_RELEASE_BRANCH}"
+echo "::set-output name=is_release_branch::${IS_RELEASE_BRANCH}"
+echo "::set-output name=release_version::${RELEASE_VERSION}"
